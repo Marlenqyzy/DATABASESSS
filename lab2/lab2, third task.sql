592b86858f14ee24e2f7a164d9016512f076b8cb
Marlenqyzy <78716452+Marlenqyzy@users.noreply.github.com>
2021-09-23 17:55:12 +0600

    Add files via upload

---
+++ lab2/lab2, third task.sql
@@ -0,0 +1,43 @@
+a)
+create table students (
+	full_name VARCHAR(50),
+	age INT,
+	birth_of_date DATE,
+	gender VARCHAR(50),
+	average_grade NUMERIC(2, 2),
+	info_about_yourself VARCHAR(50),
+	need_for_dormitory VARCHAR(10),
+	additional_info VARCHAR(100)
+);
+insert into students (full_name, age, birth_of_date, gender, average_grade, info_about_yourself, need_for_dormitory, additional_info) values ('Spense McCleod', 17, '12/2/2003', 'Female', 2.82, 'Transitional Justice', true, 'singer');
+insert into students (full_name, age, birth_of_date, gender, average_grade, info_about_yourself, need_for_dormitory, additional_info) values ('Xymenes Wilshaw', 20, '9/4/2001', 'Male', 3.13, 'IBC', true, 'dancer');
+insert into students (full_name, age, birth_of_date, gender, average_grade, info_about_yourself, need_for_dormitory, additional_info) values ('Brendan McKissack', 21, '6/14/2000', 'Female', 4.00, 'NX-OS', false, 'love politics');
+insert into students (full_name, age, birth_of_date, gender, average_grade, info_about_yourself, need_for_dormitory, additional_info) values ('Abeu Menichi', 17, '3/20/2004', 'Male', 3.83, 'Disability Insurance', true);
+insert into students (full_name, age, birth_of_date, gender, average_grade, info_about_yourself, need_for_dormitory, additional_info) values ('Ninette Aiton', 18, '6/7/2003', 'Female', 3.05, 'Job Descriptions', false, 'singer');
+
+b)
+create table instructors (
+	full_name VARCHAR(50),
+	speaking_languages VARCHAR(50),
+	work_experience INT,
+	possibility_to_remote_lessons VARCHAR(50)
+);
+insert into instructors (full_name, speaking_languages, work_experience, possibility_to_remote_lessons) values ('Nate Gibbon', 'Catalan', 5, true);
+insert into instructors (full_name, speaking_languages, work_experience, possibility_to_remote_lessons) values ('Vivien Petrasch', 'Swedish', 2, true);
+insert into instructors (full_name, speaking_languages, work_experience, possibility_to_remote_lessons) values ('Barrie Verryan', 'Danish', 1, false);
+insert into instructors (full_name, speaking_languages, work_experience, possibility_to_remote_lessons) values ('Emelen Goomes', 'Albanian', 4, false);
+insert into instructors (full_name, speaking_languages, work_experience, possibility_to_remote_lessons) values ('Hillel Acreman', 'Northern Sotho', 10, true);
+
+
+c)
+create table lesson_participants (
+	lesson_title VARCHAR(50),
+	teaching_instructor VARCHAR(50),
+	studying_students VARCHAR(50),
+	room_number int
+);
+insert into lesson_participants (lesson_title, teaching_instructor, studying_students, room_number) values ('Math', 'Marji Fyrth', 'Faythe Sturrock', 27);
+insert into lesson_participants (lesson_title, teaching_instructor, studying_students, room_number) values ('Databases', 'Hedi Caswell', 'Augustine Kopecka', 86);
+insert into lesson_participants (lesson_title, teaching_instructor, studying_students, room_number) values ('Algorithms', 'Noel Szymaniak', 'Rosanna McGuiness', 100);
+insert into lesson_participants (lesson_title, teaching_instructor, studying_students, room_number) values ('English', 'Arlen Quarrie', 'Saudra Turk', 454);
+insert into lesson_participants (lesson_title, teaching_instructor, studying_students, room_number) values ('Literature', 'Emilia Yancey', 'Gerta Gulliman', 125);
---
+++ lab2/lab2,, topic2.sql
@@ -0,0 +1,60 @@
+create table costumers (
+	Id INT,
+	full_name VARCHAR(50),
+	timestamp VARCHAR(50),
+	delivery address VARCHAR(50),
+	primary key(Id),
+
+);
+insert into costumers (Id, full_name, timestamp, delivery address) values (1, 'Coleen Crichten', '11:42 PM', '9 Kim Point');
+insert into costumers (Id, full_name, timestamp, delivery address) values (2, 'Mair MattiCCI', '6:05 PM', '5 Straubel Parkway');
+insert into costumers (Id, full_name, timestamp, delivery address) values (3, 'Wilmar Mabey', '3:35 PM', '70417 Arkansas Drive');
+insert into costumers (Id, full_name, timestamp, delivery address) values (4, 'Royal Muckart', '1:19 PM', '48992 Parkside Street');
+insert into costumers (Id, full_name, timestamp, delivery address) values (5, 'Myrilla Caplan', '6:22 AM', '1582 Dunning Plaza');
+UPDATE costumers SET timestamp = '10:30 PM' WHERE Id = 1;
+DELETE FROM costumers WHERE Id = 1;
+
+create table orders (
+	code VARCHAR(50),
+	costumer_id INT,
+	total_sum INT,
+	is_paid VARCHAR(50),
+	primary key(code),
+	foreign key(costumer_id) references customers
+);
+insert into orders (code, costumer_id, total_sum, is_paid) values ('24385-924', 1, 1, true);
+insert into orders (code, costumer_id, total_sum, is_paid) values ('66685-1011', 2, 2, true);
+insert into orders (code, costumer_id, total_sum, is_paid) values ('68703-111', 3, 3, true);
+insert into orders (code, costumer_id, total_sum, is_paid) values ('49738-723', 4, 4, true);
+insert into orders (code, costumer_id, total_sum, is_paid) values ('49884-071', 5, 5, false);
+UPDATE orders SET is_paid = false WHERE total_sum > 3
+DELETE FROM orders WHERE total_sum = 1;
+
+
+create table order_items (
+	order_code INT,
+	product_id INT,
+	quantity INT
+    foreign key(order_code) references orders,
+    foreign key(product_id) references products
+);
+insert into order_items (order_code, product_id, quantity) values (1, 1, 1);
+insert into order_items (order_code, product_id, quantity) values (2, 2, 2);
+insert into order_items (order_code, product_id, quantity) values (3, 3, 3);
+insert into order_items (order_code, product_id, quantity) values (4, 4, 4);
+insert into order_items (order_code, product_id, quantity) values (5, 5, 5);
+
+
+create table products (
+	Id INT,
+	name VARCHAR(50),
+	description VARCHAR(50),
+	price VARCHAR(50),
+	primary key(Id),
+
+);
+insert into products (Id, name, description, price) values (10, 'Tanny', 'Vinyl', '$3.83');
+insert into products (Id, name, description, price) values (27, 'Drusie', 'Aluminum', '$1.80');
+insert into products (Id, name, description, price) values (24, 'Gayleen', 'Wood', '$2.16');
+insert into products (Id, name, description, price) values (41, 'Thom', 'Rubber', '$3.67');
+insert into products (Id, name, description, price) values (15, 'Pyotr', 'Stone', '$4.30');
